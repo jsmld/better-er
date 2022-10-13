e004c7419ba650dbd8e3f55a010d2790c52b5ee4
Sam van Erpers Roijaards <samvanerpersroijaards@gmail.com>
2022-10-13 16:29:34 +1300

    Hello world with background color

---
+++ src/pages/_app.tsx
@@ -0,0 +1,20 @@
+// src/pages/_app.tsx
+import "tailwindcss/tailwind.css";
+import '../styles/global.css';
+import { SessionProvider } from "next-auth/react";
+import type { Session } from "next-auth";
+import type { AppType } from "next/app";
+import { trpc } from "../utils/trpc";
+
+const MyApp: AppType<{ session: Session | null }> = ({
+  Component,
+  pageProps: { session, ...pageProps },
+}) => {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
+};
+
+export default trpc.withTRPC(MyApp);
---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from "next";
+const Home: NextPage = () => {
+  return (
+    <div>Hello World</div>
+  );
+};
+
+export default Home;
